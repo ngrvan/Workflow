354bdb6c9f23c13c19f5cb50864da424a45e31f3
neyos001 <nejerfan.yosef001@stud.fh-dortmund.de>
2023-07-02 17:57:07 +0200

    Worddtei erweitert

--- db/connect.js
+++ db/connect.js
@@ -1,7 +1,7 @@
 const mongoose=require("mongoose");
 
 const username="nejerfanyosef001";
-const password="k0d0uvz1AZNarbjw";
+const password=process.env.password;
 const database="alldata";
 
 const connectionString=`mongodb+srv://${username}:${password}@cluster0.ggdpcne.mongodb.net/${database}?retryWrites=true&w=majority`;
